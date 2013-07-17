3cd247a865fafb34bbce94f23c4e8d3726d43fd2
Colin Rice <colin@daedrum.net>
2013-07-17 16:20:11 -0400

    Don't mess with permissions

--- manifests/dir.pp
+++ manifests/dir.pp
@@ -1,7 +1,6 @@
 class observatory::dir {
     file {"/var/www/Observatory/":
         ensure  => "directory",
-        mode    => 0644,
         recurse => true,
         owner   => www-data,
         group   => www-data,
