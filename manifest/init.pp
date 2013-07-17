9b91257ccfeb88c7b804683184a7147b4e7a6f5f
Colin Rice <colin@daedrum.net>
2013-07-17 13:35:29 -0400

    Quick and dirty observatory package

---
+++ manifest/init.pp
@@ -0,0 +1,14 @@
+class observatory {
+    package {
+        "python-pip":
+            ensure  => latest
+        "git":
+            ensure  => latest
+    }
+
+    exec {"/usr/bin/pip install -r /var/www/Observatory/observatory/requirements.txt":
+        alias => "pip_install"}
+
+    Package["python-pip"] -> Exec["pip_install"]
+
+}
