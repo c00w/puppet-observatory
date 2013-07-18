d879371d681ae099bd697fed05967d305f7dcb48
Colin Rice <colin@daedrum.net>
2013-07-18 12:39:42 -0400

    Cleanups, explicitly create directories

--- manifests/dir.pp
+++ manifests/dir.pp
@@ -1,5 +1,19 @@
-class observatory::dir ($source = "/vagrant") {
+class observatory::dir ($source = "/vagrant/observatory") {
+    file {"/var/www":
+        ensure => directory,
+        owner   => www-data,
+        group   => www-data,
+        mode    => 0644,
+    }
+
     file {"/var/www/Observatory/":
+        ensure  => "directory",
+        owner   => www-data,
+        group   => www-data,
+        mode    => 0644,
+    }
+
+    file {"/var/www/Observatory/observatory":
         ensure  => "directory",
         recurse => true,
         owner   => www-data,
--- manifests/ps.pp
+++ manifests/ps.pp
@@ -8,6 +8,7 @@ class observatory::ps {
 
     exec {"/usr/bin/createdb observatory -E UTF8 -l en_US.UTF8 -T template0":
         user    => "postgres",
+        unless  => "/usr/bin/psql -l | /bin/grep observatory",
         require => Package["postgresql-9.1"],
         returns => [0, 1],
     }
@@ -15,5 +16,7 @@ class observatory::ps {
     exec {"/usr/bin/psql -c \"ALTER USER postgres PASSWORD 'zaq12wsxcde34rfv'\"":
         user    => "postgres",
         require => Package["postgresql-9.1"],
+        subscribe => Package["postgresql-9.1"],
+        refreshonly => true,
     }
 }
